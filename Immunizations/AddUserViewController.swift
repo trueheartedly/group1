46db4322dcb2b7752a863d2f1d41fcdd4dbd9c0d
trueheartedly <trueheartedly@gmail.com>
2015-10-25 21:08:53 -0700

    day 2

--- Immunizations/AddUserViewController.swift
+++ Immunizations/AddUserViewController.swift
@@ -8,93 +8,196 @@
 
 import UIKit
 
-class AddUserViewController: UIViewController {
+
+// NOTE: GLOBAL VARIABLES DEFINED IN GLOBAL.SWIFT
+
+class AddUserViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    // add photo
+    // photo picker
     @IBOutlet weak var didPressAddPhoto: UIButton!
-    
+    @IBOutlet weak var photoImageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+//--TO CONSIDER: IMPLENTING OPTION TO TAKE PHOTO VS. CHOOSE IMAGE
+
+    // date picker
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var datePickerContainer: UIView!
     
-    // text inputs
+    // name text fields
+    @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var firstNameTextFieldBG: UIView!
-    @IBOutlet weak var middleInitialTextField: UIView!
-    @IBOutlet weak var lastNameTextField: UIView!
-
+    @IBOutlet weak var middleInitialTextField: UITextField!
+    @IBOutlet weak var middleInitialTextFieldBG: UIView!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextFieldBG: UIView!
     
+    // birthdate date picker
+    @IBOutlet weak var birthdateButton: UIButton!
+    @IBOutlet weak var datePickerView: UIView!
+    
+    // sex picker
+    @IBOutlet weak var sexButton: UIButton!
+    
+//    let sex = [
+//        ["M","F"],
+//    ]
+    
+    // remind me notification toggle
     @IBOutlet weak var remindMeButton: UIButton!
     
-    @IBOutlet weak var datePickerView: UIView!
+    
 
+
+    
+    
+    
+// VIEW DID LOAD ------------------------------------------------------------------
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        datePickerContainer.hidden = true
-        
+        imagePicker.delegate = self
+
+
+        // mask photo as a circle
+        photoImageView.layer.cornerRadius = photoImageView.frame.size.height/2
+        photoImageView.layer.masksToBounds = true
+        photoImageView.layer.borderWidth = 0
         
         // round corners on input fields
         firstNameTextFieldBG.layer.cornerRadius = 8
         middleInitialTextField.layer.cornerRadius = 8
         lastNameTextField.layer.cornerRadius = 8
+        birthdateButton.layer.cornerRadius  = 8
+        sexButton.layer.cornerRadius = 8
 
-        
-        
+        // hide date picker
+        datePickerContainer.hidden = true
+    
         datePicker.addTarget(self, action: Selector("datePickerChanged:"), forControlEvents: UIControlEvents.ValueChanged)
         
-        
-        
-        
-        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.ExtraLight)
+        // date picker blurring
+// TO DO: MAKE BETTER
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Light)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
         datePickerView.addSubview(blurEffectView)
-        
-        
     }
 
     
     
+    
+    
+// CUSTOM METHODS ------------------------------------------------------------------
+
+    
+    // PHOTO ------------------------------------------------------------------
+    
+    // on press add photo
+    @IBAction func didPressAddPhoto(sender: UIButton) {
+        // print("add photo button pressed")
+        
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+        // change button label to edit
+        didPressAddPhoto.setTitle("Edit Photo", forState: .Normal)
+    }
+    
+    // set selected image
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!) {
+        let selectedImage : UIImage = image
+        photoImageView.image = selectedImage
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // hide choose image modal
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    // BIRTHDATE ------------------------------------------------------------------
+    
+    // on select of date
     func datePickerChanged(datePicker:UIDatePicker) {
         let dateFormatter = NSDateFormatter()
-        
-        dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle
-        
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
         let strDate = dateFormatter.stringFromDate(datePicker.date)
         dateLabel.text = strDate
     }
-    
-    
-    
-    
-    
-    @IBAction func didPressRemindMe(sender: UIButton) {
-        remindMeButton.selected = !remindMeButton.selected
-    }
-    
-    
-    
-    @IBAction func didPressAddPhoto(sender: UIButton) {
-        print("add photo button pressed")
-    }
-    
 
-    
-    @IBAction func didPressNext(sender: UIButton) {
-        print("next button pressed")
-        // consider enabled/disabled state for next button based on fields being successfully input
-    }
-    
-    
-    
+    // on press of birthdate
     @IBAction func didPressBirthdate(sender: UIButton) {
-            datePickerContainer.hidden = false
+        datePickerContainer.hidden = false
     }
     
-    
+    // on press of done date picker
     @IBAction func didPressDatePickerDone(sender: UIButton) {
         datePickerContainer.hidden = true
     }
+    
+    
+    // REMIND ME ------------------------------------------------------------------
+    
+//--TO IMPLEMENT
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //    func pickerView(sexPicker: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    //        return sex.count
+    //    }
+    
+    
+    
+    // REMIND ME ------------------------------------------------------------------
+    
+    // on press of remind me
+    @IBAction func didPressRemindMe(sender: UIButton) {
+        remindMeButton.selected = !remindMeButton.selected
+        userReceiveReminders = !userReceiveReminders
+        print(userReceiveReminders)
+    }
+    
+    
+
+    // NEXT (FINISH) ------------------------------------------------------------------
+
+    @IBAction func didPressNext(sender: UIButton) {
+        print("next button pressed")
+        // consider enabled/disabled state for next button based on fields being successfully input
+        
+        // set global variables for use on subsequent screens
+        userFirstName = firstNameTextField.text
+        userMiddleInitial = middleInitialTextField.text
+        userLastName = lastNameTextField.text
+        userBirthdate = dateLabel.text
+        //userSex = sexLabel.text
+        
+// TO IMPLEMENT:
+        //userPhotoImage = photoImageView.image
+        
+    }
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -102,12 +205,6 @@ class AddUserViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    
-    
-    
-    
 
     /*
     // MARK: - Navigation
---
+++ Immunizations/global.swift
@@ -0,0 +1,17 @@
+//
+//  global.swift
+//  Immunizations
+//
+//  Created by James Taylor on 10/25/15.
+//  Copyright © 2015 Group 1. All rights reserved.
+//
+
+import Foundation
+
+var userReceiveReminders: Bool! = true
+var userPhotoImage: String?
+var userFirstName: String?
+var userMiddleInitial: String?
+var userLastName: String?
+var userBirthdate: String?
+var userSex: String?
